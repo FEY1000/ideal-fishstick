fc6e1d72a75f31ab945b175ef1a8fedfdab1cef0
FEY1000 <FEY1000@users.noreply.github.com>
2023-04-22 17:17:14 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="FEY1000"
+original_name="ideal_fishstick"
+original_urlname="ideal-fishstick"
+original_description="Awesome ideal_fishstick created by FEY1000"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv ideal_fishstick $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
